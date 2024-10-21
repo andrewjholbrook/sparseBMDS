017644188b54fe26c631c1bb41bf19d4c5ca81e1
15shethami <amisheth26@g.ucla.edu>
2024-10-21 10:28:36 -0700

    Rscripts for computing model sensitivity

--- code/Rscripts/HD_mse.R
+++ code/Rscripts/HD_mse.R
@@ -5,9 +5,10 @@ print(args)
 items <- as.integer(args[1])
 band <- as.double(args[2])
 sd_opt <- as.double(args[3])
-iter <- as.double(args[4])
-thinner <- as.double(args[5])
-burnin <- as.double(args[6])
+HD_dim <- as.integer(args[4])
+iter <- as.double(args[5])
+thinner <- as.double(args[6])
+burnin <- as.double(args[7])
 
 # oFile <- paste("errresults/", "sd_", sd_opt, "band#_", band, ".txt", sep="")
 
@@ -15,8 +16,10 @@ burnin <- as.double(args[6])
 library(MassiveMDS)
 library(invgamma)
 library(mvtnorm)
-library(tidyverse)
+#library(tidyverse)
+#library(dplyr)
 library(truncnorm)
+library(readr)
 
 # load necessary functions
 #source("~/sBMDS_scripts/sBMDS_MassiveMDS_functions.R")
@@ -28,15 +31,11 @@ set.seed(12345)
 #mse_band_hmc <- c()
 
 # simulate data
-Z.sim <- mvtnorm::rmvnorm(items, mean = rep(0, 2), sigma = diag(2)) 
+Z.sim <- mvtnorm::rmvnorm(items, mean = rep(0, HD_dim), sigma = diag(HD_dim)) 
 D.sim <- as.matrix(dist(Z.sim)) # n x n matrix of distances
-# noise <- matrix(rtruncnorm(items^2, mean = 0, sd = sd_opt, a = 0), items, items) 
-# noise <- (noise + t(noise)) / 2
-# diag(noise) <- 0
 noise <- matrix(0, nrow = items, ncol = items)
-noise[upper.tri(noise)] <- rtruncnorm(items * (items - 1) / 2, mean = 0, 
-                                      sd = sd_opt, a = 0)
-noise <- (noise + t(noise))
+noise[upper.tri(noise)] <- rtruncnorm(items * (items - 1) / 2, mean = 0, sd = sd_opt, a = 0)
+noise <- noise + t(noise)
 D.noise <- noise + D.sim
 
 # run simulation
@@ -46,9 +45,9 @@ D.noise <- noise + D.sim
 #                                 targetAccept = 0.65, stepSize = .1, thin = thinner)
 
 sim <- bmds_hmc(maxIts = iter, dims = 2, data = D.noise, bandwidth = band,
-                landmarks = TRUE,
+                landmarks = TRUE, 
                 targetAccept = .65, targetAccept_Sigma = 0.44, 
-                stepSize = .01, stepSizeSigma = .001, 
+                stepSize = .01, stepSizeSigma = .005, 
                 thin = thinner, burnin = burnin)
 
 # calculate mse and relative error for latent variables and sigma respectively
@@ -64,18 +63,19 @@ for (t in 1:(dim(sim$latent)[1])){
   }
 }
 
-rel_sigma <- c()
-for (t in 1:length(sim$sigma)){
-  #subset <- sample(1000)
-  rel_sigma[t] <- abs(sim$sigma[t] - sd_opt) / (sd_opt)
-}
+#rel_sigma <- c()
+#for (t in 1:length(sim$sigma)){
+#subset <- sample(1000)
+#rel_sigma[t] <- abs(sim$sigma[t] - sd_opt) / (sd_opt)
+#}
 
 #mse_band_hmc <-  mean(error) #mean(error[-(1:burnin)])
 
-error_results <- tibble(sd = sd_opt, locations = items, band.no = band, 
-                        mse = mean(error), rel_sigma = median(rel_sigma),
-                        AR = sim$AR, ARS = sim$ARS)
+error_results <- data.frame(sd = sd_opt, locations = items, band.no = band, 
+                            HD = HD_dim, mse = mean(error), 
+                            rel_sigma = median(sim$sigma),
+                            AR = sim$AR, ARS = sim$ARS)
 
 #write.csv(df_results, file = oFile, sep = ",", row.names = FALSE)
 
-write_csv(error_results, append = TRUE, file = "~/sBMDS_results/mse_results_wsigma.txt")
+write_csv(error_results, append = TRUE, file = "~/sBMDS_results/mse_HDLM.txt")
